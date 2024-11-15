b017d92dc32c84442b1a0441fa7700994d0e9d62
ritikarawat25 <160164311+ritikarawat25@users.noreply.github.com>
2024-11-16 00:27:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(".carousel-one").owlCarousel({
+        loop: true,
+        margin: 50,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+    $(".carousel-two").owlCarousel({
+        loop: true,
+        margin: 100,
+        nav: true,
+        navText: ["<div class='nav-btn prev-slide'>&lt;</div>", "<div class='nav-btn next-slide'>&gt;</div>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+});
